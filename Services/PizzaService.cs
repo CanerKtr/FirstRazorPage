f8c128f5c89bdb2a181c2210da63dc19904d164f
Salim Caner Kitir <ckitir.dev@gmail.com>
2025-11-21 16:44:32 +0300

    Add get, add, and delete pizza features

---
+++ Services/PizzaService.cs
@@ -0,0 +1,45 @@
+﻿using WebApplication1.Models;
+using WebApplication1.Data;
+namespace WebApplication1.Services
+{
+    public class PizzaService
+    {
+        private readonly WebAppDbContext _context = default!;
+
+        public PizzaService(WebAppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Pizza> GetPizzas()
+        {
+            if (_context.Pizzas != null)
+            {
+                return _context.Pizzas.ToList();
+            }
+            return new List<Pizza>();
+        }
+
+        public void AddPizza(Pizza pizza)
+        {
+            if (_context.Pizzas != null)
+            {
+                _context.Pizzas.Add(pizza);
+                _context.SaveChanges();
+            }
+        }
+
+        public void DeletePizza(int id)
+        {
+            if (_context.Pizzas != null)
+            {
+                var pizza = _context.Pizzas.Find(id);
+                if (pizza != null)
+                {
+                    _context.Pizzas.Remove(pizza);
+                    _context.SaveChanges();
+                }
+            }
+        }
+    }
+}
